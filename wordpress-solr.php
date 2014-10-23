4e01220f5ce4203449d82d4826a60b1c10cea36b
Paul Vorbach <paul@vorba.ch>
2014-10-23 12:02:41 +0200

    Plugin information

---
+++ wordpress-solr.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WordPress Solr by PALASTHOTEL
+Description: Use the Apache Solr search engine in WordPress.
+Version: 0.0.1
+Author: Palasthotel GmbH
+Author URI: http://palasthotel.de/
+Plugin URI: https://github.com/palasthotel/wordpress-solr
+License: GPL3
+*/
