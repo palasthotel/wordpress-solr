fc7791fbc9ff0c13c29ea3fce7c2c24aedae7327
Paul Vorbach <paul@vorba.ch>
2014-11-06 16:35:39 +0100

    More configuration parameters

--- config.sample.php
+++ config.sample.php
@@ -3,7 +3,6 @@
  * This is an example configuration file. Change it according to your needs and
  * rename it to config.php.
  */
-
 $solarium_config = array(
   'endpoint' => array(
     array(
@@ -15,5 +14,11 @@ $solarium_config = array(
 );
 
 $phsolr_config = array(
-  'docs_per_indexing_op' => 50
+  // number of documents (posts, pages, comments) per index update
+  'posts_per_index_update' => 50,
+  'pages_per_index_update' => 50,
+  'comments_per_index_update' => 50,
+
+  // how often should the index be updated
+  'update_interval' => 'hourly'
 );
