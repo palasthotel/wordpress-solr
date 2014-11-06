cbcca8caf301cbfe4a9d79704d3969deb77ba2ca
Paul Vorbach <paul@vorba.ch>
2014-11-06 15:29:52 +0100

    Sample configuration file

---
+++ config.sample.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This is an example configuration file. Change it according to your needs and
+ * rename it to config.php.
+ */
+
+$solarium_config = array(
+  'endpoint' => array(
+    array(
+      'host' => '127.0.0.1',
+      'port' => 8983,
+      'path' => '/solr/'
+    )
+  )
+);
