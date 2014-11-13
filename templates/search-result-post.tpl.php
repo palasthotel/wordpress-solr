ae9bf03b7e413d39433136ba71e27ca2038a4d5c
Paul Vorbach <paul@vorba.ch>
2014-11-13 18:16:50 +0100

    Simple default templates

--- templates/search-result-post.tpl.php
+++ templates/search-result-post.tpl.php
@@ -1,1 +1,20 @@
+<article id="post-<?php echo explode('/',$phsolr_document->id)[1]; ?>"
+  class="post-19 post type-post status-publish format-standard hentry category-uncategorized">
+
+  <header class="entry-header">
+    <h1 class="entry-title">
+      <a href="<?php echo $phsolr_document->url ?>"><?php echo $phsolr_document->title ?></a>
+    </h1>
+    <div class="entry-meta">
+      <time class="entry-date" datetime="<?php echo $phsolr_document->date ?>"><?php echo explode('T',$phsolr_document->date)[0] ?></time>
+
+    </div>
+  </header>
+
+  <div class="entry-content">
 <?php
+echo $phsolr_document->content;
+?>
+  </div>
+
+</article>
--- templates/search-results.tpl.php
+++ templates/search-results.tpl.php
@@ -1,1 +1,48 @@
+<form role="search" method="get" class="search-form"
+  action="<?php echo home_url('/') ?>">
+  <input type="hidden" name="page_id"
+    value="<?php echo $phsolr_search_page_id ?>" />
+  <div>
+    <label> <span class="screen-reader-text">Search for:</span> <input
+      type="search" class="search-field"
+      placeholder="<?php echo __('Search …') ?>"
+      value="<?php echo $phsolr_search_args['text'] ?>" name="search"
+      title="Search for:" />
+    </label> <input type="submit" class="search-submit" value="Search" />
+  </div>
+  <div class="advanced-search-settings">
+    <fieldset class="cbgrp-year">
+      <legend>Year</legend>
+      <input type="checkbox" name="year-2014" id="year-2014" /> <label
+        for="year-2014">2014 (15)</label><br /> <input type="checkbox"
+        name="year-2013" id="year-2013" /> <label for="year-2013">2013 (11)</label><br />
+      <input type="checkbox" name="year-2012" id="year-2012" /> <label
+        for="year-2012">2012 (3)</label><br />
+    </fieldset>
+  </div>
+</form>
+<div id="search-results">
+  <h1>
+    <em><?php echo $phsolr_search_results->getNumFound() ?></em> Result(s) found
+    for <em>“<?php echo $phsolr_search_args['text'] ?>”</em>
+  </h1>
+<?php
+foreach ($phsolr_search_results as $doc) {
+  global $phsolr_document;
+  $phsolr_document = $doc;
+
+  $type = explode('/', $doc->id)[0];
+
+  if ($type === 'post') {
+    include __DIR__ . '/search-result-post.tpl.php';
+  } else if ($type === 'page') {
+    include __DIR__ . '/search-result-page.tpl.php';
+  } else if ($type === 'page') {
+    include __DIR__ . '/search-result-comment.tpl.php';
+  } else {
+    throw new Exception('Unknown document type: ' . $doc->type);
+  }
+}
+?>
+</div>
 <?php
