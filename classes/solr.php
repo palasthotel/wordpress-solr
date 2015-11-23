ec02eb2b8f56cd3b75bb6b4fc66b4b1c0cdc4864
Edward <edward.bock@palasthotel.de>
2015-11-23 14:48:03 +0100

    cleaned

---
+++ classes/solr.php
@@ -0,0 +1,314 @@
+<?php
+namespace SolrPlugin;
+
+class PhSolr {
+  private $client;
+
+  private $config;
+
+  private $search_args;
+
+  public function __construct(\Solarium\Client $client, array $config,
+      $search_args) {
+    $this->client = $client;
+    $this->config = $config;
+    $this->search_args = $search_args;
+  }
+
+  public function getConfiguration() {
+    return $this->config;
+  }
+
+  public function getSearchArgs() {
+    return $this->searchArgs;
+  }
+
+  private function getModifiedPosts() {
+    // get the modification time of the last indexed post
+    $page = (int) get_option('phsolr_post_index_page', 0);
+
+    // find newer posts
+    $query = new WP_Query(
+        array(
+          'post_type' => 'any',
+          'post_status' => 'any',
+          'orderby' => array('modified', 'date', 'ID'),
+          'order' => 'ASC',
+          'posts_per_page' => $this->config['posts_per_index_update'],
+          'paged' => $page,
+          'ignore_sticky_posts' => TRUE
+        ));
+
+    echo $page.'<br>';
+
+    // when posts have been found
+    if ($query->have_posts()) {
+      // remember the time
+      update_option('phsolr_post_index_page', $page + 1);
+
+      // filter posts
+      $posts = array();
+      foreach ($query->posts as $post) {
+        if (phsolr_post_filter($post))
+          $posts[] = $post;
+      }
+
+      return $posts;
+    } else {
+      return array();
+    }
+  }
+
+  private function getNewComments() {
+    // get the modification time of the last indexed comment
+    $last_comment_modified = get_option('phsolr_last_comment_modified',
+        '1970-01-01T00:00:00Z'); // default it unix epoch
+
+    return array();
+  }
+
+  private function updateIndex(array $changedItems, array $deletedItems, $type) {
+    // new update
+    $update = $this->client->createUpdate();
+
+
+    $set_fields = NULL;
+    if ($type === 'post') {
+      $set_fields = 'phsolr_set_post_fields';
+    } else if ($type === 'comment') {
+      $set_fields = 'phsolr_set_comment_fields';
+    } else {
+      throw new Exception('unknown document type');
+    }
+
+    // for each item, add a document to the update query
+    foreach ($changedItems as $item) {
+      // create a new document for the data
+      $doc = $update->createDocument();
+
+      // set the ID
+      $doc->id = $type . '/' . $item->ID;
+
+      // set the other fields
+      $set_fields($doc, $item);
+
+      // and the type
+      $doc->type = $type;
+
+      // add document to query
+      $update->addDocument($doc);
+    }
+
+    // for each deleted item
+    foreach ($deletedItems as $item) {
+      // add a delete command to the query
+      $update->addDeleteById($type . '/' . $item->ID);
+    }
+
+    // commit
+    $update->addCommit();
+
+    // execute the update
+    try {
+      $result = $this->client->update($update);
+      return $result;
+    } catch (\Solarium\Exception\HTTPException $e) {
+      die($e->getMessage());
+    }
+  }
+
+  private function getDeletedPosts() {
+//     $posts = get_posts(
+//         array(
+//           'post_status' => 'trash',
+//           'posts_per_page' => 500
+//         ));
+
+//     $pages = get_pages(
+//         array(
+//           'post_status' => 'trash',
+//           'posts_per_page' => 500
+//         ));
+
+    return array();//array_merge($posts, $pages);
+  }
+
+  private function getDeletedComments() {
+    return array();
+  }
+
+  public function updatePostIndex() {
+    $this->updateIndex($this->getModifiedPosts(), $this->getDeletedPosts(),
+        'post');
+  }
+
+  public function updateCommentIndex() {
+    $this->updateIndex($this->getNewComments(), $this->getDeletedComments(),
+        'comment');
+  }
+
+  public function resetPostIndex() {
+    // reset the last modified time, so the index will be rebuilt
+    update_option('phsolr_post_index_page', '0');
+  }
+
+  public function resetCommentIndex() {
+    update_option('phsolr_last_comment_modified', '1970-01-01T00:00:00Z');
+  }
+
+  public function optimizeIndex() {
+    // get an update query instance
+    $update = $this->client->createUpdate();
+
+    // optimize the index
+    $update->addOptimize(NULL, NULL, NULL); // use solr defaults
+
+    // this executes the query and returns the result
+    try {
+      $result = $this->client->update($update);
+      return $result;
+    } catch (\Solarium\Exception\HTTPException $e) {
+      die($e->getMessage());
+    }
+  }
+
+  public function deleteIndex() {
+    // get an update query instance
+    $update = $this->client->createUpdate();
+
+    // delete the index
+    $update->addDeleteQuery('*:*');
+
+    // this executes the query and returns the result
+    try {
+      $result = $this->client->update($update);
+      return $result;
+    } catch (\Solarium\Exception\HTTPException $e) {
+      die($e->getMessage());
+    }
+  }
+
+  /**
+   * Runs the search.
+   *
+   * @return array search results
+   */
+  public function search() {
+    if ($this->search_args === FALSE) {
+      return array();
+    }
+
+    $select = $this->client->createSelect();
+
+    // set search query
+    $query = $this->search_args['text'];
+    $select->setQuery($query);
+
+    $filter = $select->createFilterQuery('published')->setQuery('published:true');
+
+    // set query offset and limit
+    $select->setStart(($this->search_args['page'] - 1) * $this->config['query_limit']);
+    $select->setRows($this->config['query_limit']);
+
+    // set result fields
+    $select->setFields($this->config['result_fields']);
+
+    if (count($this->config['boost_functions']) > 0) {
+      $dismax = $select->getDisMax();
+      $dismax->setBoostFunctions(implode(' ', $this->config['boost_functions']));
+    }
+
+    // enable spellchecker
+    if ($this->config['spellcheck']) {
+      $spellcheck = $select->getSpellcheck();
+      $spellcheck->setQuery($query);
+      $spellcheck->setCount(10);
+      $spellcheck->setBuild(TRUE);
+      $spellcheck->setCollate(TRUE);
+      $spellcheck->setExtendedResults(TRUE);
+      $spellcheck->setCollateExtendedResults(TRUE);
+    }
+
+    // apply facets
+    if ($this->search_args['facets']) {
+      foreach ($this->search_args['facets'] as $facet_key_val => $enabled) {
+        if ($enabled) {
+          $kv = explode('-', $facet_key_val);
+          $filter_query = new \Solarium\QueryType\Select\Query\FilterQuery();
+          $filter_query->setKey(strtolower($kv[0]));
+          $filter_query->setQuery($kv[1]);
+          $select->addFilterQuery($filter_query);
+        }
+      }
+    }
+
+    // show other facets
+    if (isset($this->config['facets'])) {
+      $facetSet = $select->getFacetSet();
+
+      // content type facet
+      if (isset($this->config['facets']['type'])) {
+        $facet = $this->config['facets']['type'];
+        // type facet
+        $facetSet->createFacetField($facet['title'])->setField($facet['field'])->setMinCount(
+            1);
+      }
+
+      // year facet
+      if (isset($this->config['facets']['date'])) {
+        $facet = $this->config['facets']['date'];
+        // the date facet
+        // from epoch until now
+        $facetSet->createFacetRange($facet['title'])->setField($facet['field'])->setStart(
+            '1970-01-01T00:00:00Z')->setEnd(
+            str_replace('+00:00', 'Z', date('c')))->setGap('+1YEAR');
+      }
+    }
+
+    // weight of fields
+    $dismax = $select->getDisMax();
+
+    // build the weight string
+    $weightString = '';
+    foreach ($this->config['search_fields'] as $field => $weight) {
+      $weightString .= " $field^$weight";
+    }
+
+    $dismax->setQueryFields(substr($weightString, 1));
+
+    // set the default operator
+    $select->setQueryDefaultOperator($this->config['default_query_operator']);
+
+    $search_results = $this->client->select($select);
+
+    return $search_results;
+  }
+
+  public function showResults($search_page_id, $search_results) {
+    // make parameters global, so they can be used in the template
+    global $phsolr_search_page_id;
+    global $phsolr_search_args;
+    global $phsolr_search_results;
+    global $phsolr_search_config;
+
+    $phsolr_search_page_id = $search_page_id;
+    $phsolr_search_args = $this->search_args;
+    $phsolr_search_results = $search_results;
+    $phsolr_search_config = $this->config;
+
+    $template_file = 'search-results.php';
+
+    // theme/template paths
+    $theme = wp_get_theme();
+    $theme_dir = $theme->get_theme_root() . '/' . $theme->get_stylesheet();
+    $include_path = "$theme_dir/$template_file";
+
+    // if a custom template exists in the directory of the current template,
+    // use it, otherwise use the default template in this plugin's directory
+    if (!file_exists($include_path)) {
+      $include_path = __DIR__ . "/templates/$template_file";
+    }
+
+    include $include_path;
+  }
+}
---
+++ solr.php
@@ -0,0 +1,267 @@
+<?php
+/*
+Plugin Name: Solr
+Description: Use the Apache Solr search engine in WordPress.
+Version: 0.1.0
+Author: Palasthotel (Edward Bock, Daniel Blume)
+URI: http://palasthotel.de/
+Plugin URI: https://github.com/palasthotel/wordpress-solr
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+  die;
+}
+
+require_once dirname(__FILE__) . '/classes/solr.php';
+
+class SolrPlugin
+{
+	public $dir;
+	public $url;
+	private $solr;
+
+	/**
+	* construct grid plugin
+	*/
+	function __construct()
+	{
+		/**
+		* base paths
+		*/
+		$this->dir = plugin_dir_path(__FILE__);
+		$this->url = plugin_dir_url(__FILE__);
+
+		global $grid_loaded;
+		$grid_loaded = false;
+
+		/**
+		* settings page
+		*/
+		require('classes/settings.inc');
+		new \SolrPlugin\Settings();
+
+	}
+
+	public function get_solr(){
+		if ($this->solr === NULL) {
+			// autoload dependencies
+			require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+			// load configuration
+			if (file_exists(dirname(__FILE__) . '/config.php')) {
+				require_once dirname(__FILE__) . '/config.php';
+			} else {
+				die('Configuration file missing. Please add authentication information to' .
+					' "config.sample.php" and rename it to "config.php".');
+			}
+
+			// instantiate PhSolr
+			$this->solr = new PhSolr(new Solarium\Client($solarium_config), $phsolr_config,
+				phsolr_get_search_args());
+		}
+
+		return $this->solr;
+	}
+
+	public function save_latest_run(){
+		update_option('phsolr_post_index_run', date("Y-m-d h:i:s"));
+	}
+
+}
+global $solr_plugin;
+$solr_plugin = new SolrPlugin();
+
+/**
+ * Returns an instance of Solr.
+ *
+ * @return Solr
+ */
+function phsolr_get_instance() {
+	global $solr_plugin;
+	return $solr_plugin->get_solr();
+}
+
+function phsolr_create_search_result_page() {
+  // search for a page titled 'Search Results'
+  $page = get_page_by_title('Search Results', 'OBJECT', 'page');
+
+  if (!$page) {
+    // if it doesn't exist, create a new page
+    $page = array(
+      'post_type' => 'page',
+      'post_name' => 'search',
+      'post_title' => 'Search Results',
+      'post_content' => '[phsolr_search_results]',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_name' => 'Search Results',
+      'comment_status' => 'closed'
+    );
+
+    $page_id = wp_insert_post($page);
+  } else if ($page->post_status != 'publish') {
+    // if the page is unpublished, publish it
+    $page->post_status = 'publish';
+
+    wp_update_post($page);
+  } else {
+    // otherwise remember its ID
+    $page_id = $page->ID;
+  }
+}
+
+function phsolr_activate() {
+  $phsolr = phsolr_get_instance();
+  $config = $phsolr->getConfiguration();
+
+  phsolr_create_search_result_page();
+
+  // schedule index updates in 1 min
+  wp_schedule_event(time(), $config['posts_update_interval'],
+      'phsolr_update_post_index_event');
+  // comments are indexed 20 mins later
+  wp_schedule_event(time() + 60 * 10, $config['comments_update_interval'],
+      'phsolr_update_comment_index_event');
+
+  // optimize index
+  if ($config['optimization_interval'] !== 'never') {
+    wp_schedule_event(time() + 60 * 15, $config['optimization_interval'],
+        'phsolr_optimize_index_event');
+  }
+}
+
+function phsolr_deactivate() {
+  wp_clear_scheduled_hook('phsolr_update_post_index_event');
+  wp_clear_scheduled_hook('phsolr_update_comment_index_event');
+  wp_clear_scheduled_hook('phsolr_optimize_index_event');
+}
+
+// workaround, since register_activation_hook doesn't work with symlinks
+$__FILE__ = basename(dirname(__FILE__)) . '/' . basename(__FILE__);
+register_activation_hook($__FILE__, 'phsolr_activate');
+register_deactivation_hook($__FILE__, 'phsolr_deactivate');
+
+function phsolr_update_post_index() {
+  $phsolr = phsolr_get_instance();
+
+  $phsolr->updatePostIndex();
+}
+add_action('phsolr_update_post_index_event', 'phsolr_update_post_index');
+
+function phsolr_update_comment_index() {
+  // do nothing for now
+  // $phsolr = phsolr_get_instance();
+  //
+  // $phsolr->updateCommentIndex();
+}
+add_action('phsolr_update_comment_index_event', 'phsolr_update_comment_index');
+
+function phsolr_optimize_index() {
+  $phsolr = phsolr_get_instance();
+
+  $phsolr->optimizeIndex();
+}
+add_action('phsolr_optimize_index_event', 'phsolr_optimize_index');
+
+function phsolr_print_search_form() {
+  echo phsolr_search_form();
+}
+
+function phsolr_search_form($form) {
+  $search_args = phsolr_get_search_args();
+  $search_page_id = phsolr_get_search_page_id();
+  ob_start();
+  ?>
+<form role="search" method="get" class="search-form"
+  action="<?php echo home_url('/') ?>">
+  <input type="hidden" name="page_id" value="<?php echo $search_page_id; ?>" />
+  <div>
+    <label> <span class="screen-reader-text">Search for:</span> <input
+      type="search" class="search-field"
+      placeholder="<?php echo __('Search …') ?>"
+      value="<?php echo $search_args['text']; ?>" name="query"
+      title="Search for:" />
+    </label> <input type="submit" class="search-submit" value="Search" />
+  </div>
+</form>
+<?php
+  $form = ob_get_clean();
+  return $form;
+}
+add_filter('get_search_form', 'phsolr_search_form');
+
+function phsolr_get_search_page_id() {
+  $page = get_page_by_title('Search Results');
+  return $page->ID;
+}
+
+/**
+ * Returns the search arguments as an associative array or FALSE if there was no
+ * search.
+ *
+ * @return array
+ */
+function phsolr_get_search_args() {
+  $args = array();
+  if (isset($_GET['query'])) {
+    $args['text'] = $_GET['query'];
+  } else {
+    return FALSE;
+  }
+
+  // sanitize page param
+  if (isset($_GET['page_num'])) {
+    $args['page'] = (int) $_GET['page_num'];
+
+    if ($args['page'] < 1) {
+      $args['page'] = 1;
+    }
+  } else {
+    $args['page'] = 1;
+  }
+
+  $facet_args = array();
+  foreach ($_GET as $key => $value) {
+    if (strpos($key, 'facet-') === 0) {
+      $facet_args[substr($key, 6)] = $value === 'on';
+    }
+  }
+
+  $args['facets'] = $facet_args;
+
+  return $args;
+}
+
+// add shortcodes for use in pages
+add_shortcode('phsolr_search_form', 'phsolr_print_search_form');
+add_shortcode('phsolr_search_results', 'phsolr_print_search_results');
+
+function phsolr_print_search_results() {
+  $search_page_id = phsolr_get_search_page_id();
+
+  $phsolr = phsolr_get_instance();
+
+  if (isset($_GET['action'])) {
+    if ($_GET['action'] === 'rebuild') {
+      $phsolr->resetPostIndex();
+      $phsolr->resetCommentIndex();
+      echo '<p>Index rebuild initialized.</p>';
+    } else if ($_GET['action'] == 'optimize') {
+      $phsolr->optimizeIndex();
+      echo '<p>Index optimized</p>';
+    } else if ($_GET['action'] == 'update') {
+      $phsolr->updatePostIndex();
+      $phsolr->updateCommentIndex();
+      echo '<p>Index updated</p>';
+    } else if ($_GET['action'] === 'delete') {
+      $phsolr->deleteIndex();
+      echo '<p>Index deleted</p>';
+    } else {
+      echo '<p>Unknown action "'.$_GET['action'].'"</p>';
+    }
+  } else {
+    $search_results = $phsolr->search();
+    $phsolr->showResults($search_page_id, $search_results);
+  }
+}
