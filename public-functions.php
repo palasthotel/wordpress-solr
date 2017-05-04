a123b59b018f80d2d42c6ea3afbee3c83f603880
EdwardBock <edward.bock@palasthotel.de>
2017-05-04 15:36:54 +0200

    public search function

--- public-functions.php
+++ public-functions.php
@@ -6,3 +6,18 @@
 function solr_get_plugin(){
 	return SolrPlugin\Plugin::instance();
 }
+
+/**
+ *
+ * @param $s string
+ * @return \Solarium\QueryType\Select\Result\Result $results
+ */
+function solr_search($s){
+
+	$solarium = SolrPlugin\Solarium::instance( solr_get_plugin() );
+
+	$query = $solarium->createSelect();
+	$query->setQuery( $s );
+
+	return $solarium->execute( $query );
+}
