562fc1468d7ee54849b8febf3c03aec5759f058f
EdwardBock <edward.bock@palasthotel.de>
2017-06-16 15:17:25 +0200

    search to early for get_query_vars fix

--- inc/frontend-search.php
+++ inc/frontend-search.php
@@ -18,24 +18,8 @@ class FrontendSearch {
 			 * if enabled overwrite default search
 			 * and do the search if needed
 			 */
-			add_action( 'posts_request', array( $this, 'do_search' ), 10, 2 );
 			add_filter( 'posts_request', array( $this, 'disable_search_query' ), 10, 2 );
 		}
-	}
-
-	/**
-	 * do the search in init action
-	 * @param array $request
-	 * @param $query \WP_Query
-	 *
-	 * @return array|bool
-	 */
-	function do_search($request, $query) {
-		if ( $this->plugin->request->is_search() ) {
-			$this->plugin->request->get_search_results();
-		}
-		return $request;
-
 	}
 
 	/**
